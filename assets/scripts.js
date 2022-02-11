c28c9147efb487fd4c040b9a4b8f8e263e36ec65
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-10 22:15:14 -0500

    js listening event for button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,74 +1,34 @@
-var bob = document.querySelector(".bob");
+// Variables do var
+var inputEl = document.querySelector("#form")
+var searchButton = document.querySelector("#btn")
+var songs = document.querySelector("#songs")
 
-bob.addEventListener("click", function() {
-    bob.style.backgroundColor="pink";
-    bob.style.color = "white";
-    bob.setAttribute("id", "coolId");
-})
+var buttonClickHandler = function (event){
+event.preventDefault();
+//value needed form api search
+var songName = songs.value.trim();
+getLyrics();
 
-var createH1 = document.createElement("h1");
-createH1.textContent ="Hi there!";
-bob.append(createH1);
+};
+  
+var getLyrics = function (user) {
+  var apiUrl = "https://api.lyrics.ovh/v1/";
 
-// Window alerts
-//1. Input: prompt()
-var inputData = prompt("What's your name?");
+  fetch(apiUrl)
+  .then(function(response) {
+    if(response.ok) {
+      console.log(response);
+      response.json().then(function(data) {
+        //function call to display
+        //create modals
+      });
+    
 
-//2. True/False confirm()
-
-var trueFalse = confirm("Do you like Javascript?")
-console.log("Name: ", inputData)
-console.log("Likes coding: ", trueFalse)
-// 3. notification alert()
-alert("Hi Chloe!")
-
-// var array = ["dog", "cat", "lion", "elephant"]
-
-// for (var i=0; i < array.length; i++) {
-//     //console.log(array[i])
-//     if (array[i] ==="dog") {
-//         console.log("dog!")
-//     } else if(array[i]==="cat") {
-//         console.log("Cat!!")
-//     } else {
-//         console.log("Not a dog or cat")
-//     }
-
-
-// }
-
-// // for (var i=0; i < 2; i++) {
-// //     console.log(array[i])
-
-// // }
-
-// // for (var i=2; i < array.length; i++) {
-// //     console.log(array[i])
-
-// // }
-
-// //function() {
-//     //action
-// //}
-
-// function nameOfFunction() {
-//     if() {
-
-//     }
-// }
-
-// nameOfFunction();
+    }
+  })
+}
 
 
 
-// Window Pop Ups
-    // Notification: alert("message");
-    // True/False: confirm("message");
-    // User Input: prompt("message");
 
-
-    var createdPassword = "";
-  for(var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random()* mustChooseOne.length);
-    createdPassword = createdPassword + mustChooseOne;
-  }
+apiUrl = "https//api.lyrics.ovh";
