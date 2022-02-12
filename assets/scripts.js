f66d1ff1df56dc5d5fb46bffd659d641dda14cef
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-11 21:59:09 -0500

    first api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,21 +1,27 @@
-// Variables do var
-var inputEl = document.querySelector("#form")
-var searchButton = document.querySelector("#btn")
-var songs = document.querySelector("#songs")
+var inputEl = document.querySelector("#form");
+var searchButton = document.querySelector("#btn");
+var songs = document.querySelector("#songs");
+var artists = document.querySelector("#artist");
+
+var songName;
+var artistName;
 
 var buttonClickHandler = function (event){
 event.preventDefault();
 //value needed form api search
-var songName = songs.value.trim();
+songName = songs.value.trim();
+artistName = artists.value.trim();
 getLyrics();
 
 };
-  
+
+
 var getLyrics = function (user) {
-  var apiUrl = "https://api.lyrics.ovh/v1/";
+  var apiUrl = "https://api.lyrics.ovh/v1/" + artistName + "/" + songName ;
 
   fetch(apiUrl)
   .then(function(response) {
+    console.log(response)
     if(response.ok) {
       console.log(response);
       response.json().then(function(data) {
@@ -24,11 +30,16 @@ var getLyrics = function (user) {
       });
     
 
+    } else if (response.status === 404){
+      alert("No lyrics found!");
+
     }
+
+
+  }).catch(function(error) {
+    console.log(error)
   })
 }
 
 
-
-
-apiUrl = "https//api.lyrics.ovh";
+searchButton.addEventListener("click", buttonClickHandler);
